2cca0b74940f6c4360821ed28c86f7857448b89d
Stefanus Wilfrid <77448713+Stefanuswilfrid@users.noreply.github.com>
2023-02-14 22:52:25 +0700

    Update email.js

--- pages/api/email.js
+++ pages/api/email.js
@@ -10,7 +10,7 @@ export default async function handler(req,res){
     const {messageType,warmOrCold,personReceiving,messageReason} = req.body ;
     const prompt = `write a ${warmOrCold} ${messageType} to the person for the specified reason below. 
     Make sure the message is tailored toward the person's experience & skillset . Person: ${personReceiving}
-    Reason : ${messageReason}. Return in HTML format, no img.`;
+    Reason : ${messageReason}. Return in HTML string format, no img.`;
 
     const response = await openai.createCompletion({
       model: "text-davinci-003", 
