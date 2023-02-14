ca400e3767368201fac46d29ced12509cb2a0fec
Stefanus Wilfrid <77448713+Stefanuswilfrid@users.noreply.github.com>
2023-02-14 23:03:09 +0700

    not in html

--- pages/api/recipe.js
+++ pages/api/recipe.js
@@ -12,7 +12,7 @@ export default async function handler(req,res){
     const prompt = `write a complete meal recipe including cooking steps, meal name and ingredients . 
     For a chef with these ingredients : ${ingredients} , duration should be less than ${duration} minutes,
     and achievable using these  cooking tools :  ${isOven ? "oven," : ""} ${isBlender ? "blender," : ""} 
-    ${isMicrowave ? "microwave," : ""} ${isStove ? "stove" : ""}. return in HTML string format `;
+    ${isMicrowave ? "microwave," : ""} ${isStove ? "stove" : ""} `;
 
 
     const response = await openai.createCompletion({
